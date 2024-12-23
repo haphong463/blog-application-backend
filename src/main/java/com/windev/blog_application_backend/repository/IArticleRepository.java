4e453f3a3637235bc2f10b26249b5b470a2f19c5
haphong463 <haphong463@gmail.com>
2024-12-24 01:04:23 +0700

    REPOSITORY: get article with author

--- src/main/java/com/windev/blog_application_backend/repository/IArticleRepository.java
+++ src/main/java/com/windev/blog_application_backend/repository/IArticleRepository.java
@@ -1,12 +1,21 @@
 package com.windev.blog_application_backend.repository;
 
+import com.windev.blog_application_backend.dto.ArticleWithUser;
 import com.windev.blog_application_backend.model.Article;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
-public interface ArticleRepository extends MongoRepository<Article, String> {
-    @Query("{ $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'tags': { $regex: ?0, $options: 'i' } } ] }")
-    Page<Article> findByKeyword(String keyword, Pageable pageable);}
+public interface IArticleRepository extends MongoRepository<Article, String> {
+    @Aggregation(pipeline = {
+            "{ $match: { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'tags': { $regex: ?0, $options: 'i' } } ] } }",
+            "{ $lookup: { from: 'users', localField: 'userId', foreignField: '_id', as: 'user' } }",
+            "{ $unwind: { path: '$user', preserveNullAndEmptyArrays: true } }",
+            "{ $skip: ?#{#pageable.offset} }",
+            "{ $limit: ?#{#pageable.pageSize} }"
+    })
+    Slice<ArticleWithUser> findByKeyword(String keyword, Pageable pageable);
+
+}
--- src/main/java/com/windev/blog_application_backend/repository/IUserRepository.java
+++ src/main/java/com/windev/blog_application_backend/repository/IUserRepository.java
@@ -9,7 +9,7 @@ import com.windev.blog_application_backend.model.User;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface IUserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
